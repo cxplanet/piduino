4f67f8c0f22adaf9c4b75761439d3b9e1eef575a
Jay Lorenzo <cxplanet@hotmail.com>
2014-06-24 16:28:35 -0700

    Simple motor shield test

---
+++ arduino/motor_shield/motor_shield.ino
@@ -0,0 +1,49 @@
+
+
+void setup() {
+  
+  //Setup Channel A
+  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+  //Setup Channel B
+  pinMode(13, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
+  
+}
+
+void loop(){
+  
+  //forward @ half speed
+  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
+  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+  analogWrite(3, 123);   //Spins the motor on Channel A at full speed
+  
+  digitalWrite(13, HIGH);  //Establishes backward direction of Channel B
+  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+  analogWrite(11, 123);    //Spins the motor on Channel B at half speed
+  
+  delay(3000);
+  
+  digitalWrite(9, HIGH); //Eengage the Brake for Channel A
+  digitalWrite(8, HIGH); //Engage the Brake for Channel B
+
+  delay(1000);
+  
+  //backward @ half speed
+  digitalWrite(12, LOW); //Establishes backward direction of Channel A
+  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+  analogWrite(3, 123);   //Spins the motor on Channel A at half speed
+   digitalWrite(13, LOW);  //Establishes backward direction of Channel B
+  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+  analogWrite(11, 123);    //Spins the motor on Channel B at half speed
+  
+  delay(3000);
+  
+  digitalWrite(9, HIGH); //Eengage the Brake for Channel A
+  digitalWrite(8, HIGH); //Eengage the Brake for Channel A
+  
+  delay(1000);
+  
+}
+
+
