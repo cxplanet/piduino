989f284210acd02c3bb37ecde99e4e3ca77ae8a0
Jay Lorenzo <cxplanet@hotmail.com>
2014-09-17 07:07:32 -0700

    digital motor foo

---
+++ arduino/window_shade/window_shade.ino
@@ -0,0 +1,32 @@
+
+ #include <Servo.h>
+ 
+ // pin assignments
+ int photoResistor = 0;
+ //set this to your darkness threshold
+ int darkness = 150;
+ // set this to your 'full light' threshold
+ int light = 950;
+ Servo servo;
+ int val;
+ 
+ void setup() {
+   pinMode(photoResistor, INPUT);
+   servo.attach(9);
+   Serial.begin(9600);
+ }
+ 
+ // read from the analog input connected to the LDR
+ // and print the value to the serial port.
+ // the delay is only to avoid sending so much data
+ // as to make it unreadable.
+ void loop() {
+   val = analogRead(photoResistor);
+   Serial.println(val);
+   // map scales the servo range to the light range
+   val = map(val, darkness, light, 1, 179);
+   Serial.print("Mapped val: ");
+   Serial.println(val);
+   servo.write(val);
+   delay(1500);
+ }
